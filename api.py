e7589acaf73b346de311c0e60c9a6825516a0560
gastonledesma328-dot <gastonledesma328@gmail.com>
2026-04-25 15:12:50 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,19 +1,11 @@
 from fastapi import FastAPI
-from scraper_m3u8 import scrape_with_playwright, scrape_with_selenium, scrape_simple
+from scraper_m3u8 import scrape_with_playwright
 
 app = FastAPI()
 
 @app.get("/scrape")
 def scrape(url: str):
-    links = []
-
-    try:
-        links = scrape_with_playwright(url)
-    except:
-        try:
-            links = scrape_with_selenium(url)
-        except:
-            links = scrape_simple(url)
+    links = scrape_with_playwright(url)
 
     return {
         "status": "ok",
